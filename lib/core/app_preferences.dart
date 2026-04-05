8c2a55c92c810926e5e803fdee0b6ede4c6bc012
RoshanJ20 <roshan.john.rs@gmail.com>
2026-04-05 23:57:05 +0530

    refactor: strip AppPreferences to has_seen_intro only

--- lib/core/app_preferences.dart
+++ lib/core/app_preferences.dart
@@ -2,8 +2,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class AppPreferences {
   static const _keyHasSeenIntro = 'has_seen_intro';
-  static const _keyMontageIndex = 'montage_index';
-  static const _keyAudioPositionMs = 'audio_position_ms';
 
   SharedPreferences? _prefs;
 
@@ -19,30 +17,5 @@ class AppPreferences {
   Future<void> setHasSeenIntro(bool value) async {
     _prefs ??= await SharedPreferences.getInstance();
     await _prefs!.setBool(_keyHasSeenIntro, value);
-  }
-
-  Future<int> get montageIndex async {
-    _prefs ??= await SharedPreferences.getInstance();
-    return _prefs!.getInt(_keyMontageIndex) ?? 0;
-  }
-
-  Future<void> setMontageIndex(int index) async {
-    _prefs ??= await SharedPreferences.getInstance();
-    await _prefs!.setInt(_keyMontageIndex, index);
-  }
-
-  Future<int> get audioPositionMs async {
-    _prefs ??= await SharedPreferences.getInstance();
-    return _prefs!.getInt(_keyAudioPositionMs) ?? 0;
-  }
-
-  Future<void> setAudioPositionMs(int ms) async {
-    _prefs ??= await SharedPreferences.getInstance();
-    await _prefs!.setInt(_keyAudioPositionMs, ms);
-  }
-
-  Future<void> clear() async {
-    _prefs ??= await SharedPreferences.getInstance();
-    await _prefs!.clear();
   }
 }
--- test/app_preferences_test.dart
+++ test/app_preferences_test.dart
@@ -28,43 +28,5 @@ void main() {
       await prefs.setHasSeenIntro(false);
       expect(await prefs.hasSeenIntro, isFalse);
     });
-
-    test('montageIndex defaults to 0', () async {
-      final prefs = AppPreferences();
-      await prefs.init();
-      expect(await prefs.montageIndex, equals(0));
-    });
-
-    test('setMontageIndex persists value', () async {
-      final prefs = AppPreferences();
-      await prefs.init();
-      await prefs.setMontageIndex(7);
-      expect(await prefs.montageIndex, equals(7));
-    });
-
-    test('audioPositionMs defaults to 0', () async {
-      final prefs = AppPreferences();
-      await prefs.init();
-      expect(await prefs.audioPositionMs, equals(0));
-    });
-
-    test('setAudioPositionMs persists value', () async {
-      final prefs = AppPreferences();
-      await prefs.init();
-      await prefs.setAudioPositionMs(45000);
-      expect(await prefs.audioPositionMs, equals(45000));
-    });
-
-    test('clear resets all values', () async {
-      final prefs = AppPreferences();
-      await prefs.init();
-      await prefs.setHasSeenIntro(true);
-      await prefs.setMontageIndex(5);
-      await prefs.setAudioPositionMs(12000);
-      await prefs.clear();
-      expect(await prefs.hasSeenIntro, isFalse);
-      expect(await prefs.montageIndex, equals(0));
-      expect(await prefs.audioPositionMs, equals(0));
-    });
   });
 }
