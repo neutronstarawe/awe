b8c4807a7fd070829beffa3c3678e9ffe8477e92
RoshanJ20 <roshan.john.rs@gmail.com>
2026-04-05 23:58:52 +0530

    refactor: make AppPreferences.init() the sole init path (remove lazy fallback)

--- lib/core/app_preferences.dart
+++ lib/core/app_preferences.dart
@@ -3,19 +3,17 @@ import 'package:shared_preferences/shared_preferences.dart';
 class AppPreferences {
   static const _keyHasSeenIntro = 'has_seen_intro';
 
-  SharedPreferences? _prefs;
+  late SharedPreferences _prefs;
 
   Future<void> init() async {
     _prefs = await SharedPreferences.getInstance();
   }
 
   Future<bool> get hasSeenIntro async {
-    _prefs ??= await SharedPreferences.getInstance();
-    return _prefs!.getBool(_keyHasSeenIntro) ?? false;
+    return _prefs.getBool(_keyHasSeenIntro) ?? false;
   }
 
   Future<void> setHasSeenIntro(bool value) async {
-    _prefs ??= await SharedPreferences.getInstance();
-    await _prefs!.setBool(_keyHasSeenIntro, value);
+    await _prefs.setBool(_keyHasSeenIntro, value);
   }
 }
